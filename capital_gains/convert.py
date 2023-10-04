ac128f408c8f34a9256318bce3924856a6295ca8
Erick Cantu Paz <cantupaz@yahoo.com>
2023-10-04 16:11:03 +0000

    adding convert.py

---
+++ capital_gains/convert.py
@@ -0,0 +1,60 @@
+"""Converts from raw transactions to a format like the one from the tax center"""
+
+import csv
+import datetime as dt
+
+TRANS_TO_ORDERS = {
+    "option expiration": "option expire",
+    "option assignment": "option assignment",
+    "sold to close": "sell to close",
+    "sold short": "sell to open",
+    "bought to open": "buy open",
+    "bought to cover": "buy to close",
+    "bought": "buy",
+    "sold": "sell",
+}
+
+IGNORE_TRANS = ("adjustment", "dividend", "interest",
+                "reorganization", "transfer")
+
+OUT_HEADER = ["Trade Date", "Order Type", "Security", "Cusip",
+              "Transaction Description", "Quantity", "Executed Price", "Commission", "Net Amount"]
+
+
+def main():
+    in_filename = "transactions-20220101-20231004.csv"
+    out_filename = "orders-20220101-20231004.csv"
+
+    with open(in_filename, "r", encoding="utf-8") as in_file:
+        reader = csv.DictReader(in_file, delimiter=",")
+
+        next(reader)
+
+        new_file = []
+        for line in reader:
+            if line["TransactionType"].lower() in IGNORE_TRANS:
+                continue
+
+            in_date = dt.datetime.strptime(line["TransactionDate"], "%m/%d/%y")
+            out_date = in_date.strftime("%m/%d/%Y")
+            new_line = (out_date,
+                        TRANS_TO_ORDERS[line["TransactionType"].lower()],
+                        line["Symbol"], "",
+                        line["Description"],
+                        line["Quantity"],
+                        line["Price"],
+                        line["Commission"],
+                        line["Amount"])
+            new_file.append(new_line)
+
+    new_file.sort()
+
+    # write to the output file
+    with open(out_filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(OUT_HEADER)
+        writer.writerows(new_file)
+
+
+if __name__ == "__main__":
+    main()
