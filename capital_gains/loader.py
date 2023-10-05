49e759bb16c84121f56cb44b9d67601529f4b54c
Erick Cantu Paz <cantupaz@yahoo.com>
2023-10-05 15:05:50 +0000

    sort the input file. s/shares/quantity

--- capital_gains/loader.py
+++ capital_gains/loader.py
@@ -4,6 +4,7 @@ import collections
 import csv
 import datetime as dt
 import decimal
+import functools
 import logging
 from model import Transaction, Lot
 from const import SHARES_PER_CONTRACT
@@ -13,11 +14,14 @@ OPTION_TRANSACTIONS = (
     "option expire",
     "option assignment",
     "sell to close",
+    "sell to open",
+    "buy to close",
 )
 SHORT_OPTION_TRANSACTIONS = (
     "sell to open",
     "buy to close",
 )
+
 
 OPEN_LOT_TRANSACTIONS = (
     "buy",
@@ -33,10 +37,29 @@ CLOSE_LOT_TRANSACTIONS = (
 )
 
 
+def compare_function(item1, item2):
+    """Compare function to sort the input file."""
+
+    # first sort by date
+    date_comp = item1[0] - item2[0]
+    if date_comp.days != 0:
+        return date_comp.days
+
+    # if same date, then ensure that transactions that open lots are sorted
+    # before transactions that close them
+    # (they can be out of order if both happen the same day)
+    comp = 0
+    if (item1[1] in OPEN_LOT_TRANSACTIONS) and (item2[1] in CLOSE_LOT_TRANSACTIONS):
+        comp = -1
+    elif (item1[1] in CLOSE_LOT_TRANSACTIONS) and (item2[1] in OPEN_LOT_TRANSACTIONS):
+        comp = 1
+    return comp
+
+
 # Reads transactions from a csv file.
 # To represent the starting portfolio, we need to add some lines with
 # purchase transactions to the file. Those will get processed and added as Lots
-
+# alternatively, we can implement changes to have a separate file with the portfolio
 
 def load_transactions(filename: str, fiscal_year: int = 0):
     """Returns dictionaries with open lots and sales. The dictionary keys are symbols."""
@@ -50,46 +73,42 @@ def load_transactions(filename: str, fiscal_year: int = 0):
         # Skip header
         next(reader)
 
-        for index, (
-            date,
-            order_type,
-            symbol,
-            cusip,
-            desc,
-            shares,
-            price,
-            fee,
-            net,
-        ) in enumerate(reader):
+        rows = []
+        for (date, order_type, symbol, cusip, desc, quantity, price, fee, net) in reader:
             date = dt.datetime.strptime(date, "%m/%d/%Y").date()
             name = None
-            shares = decimal.Decimal(shares)
-            price = decimal.Decimal(price)
+            quantity = decimal.Decimal(quantity)
+
             if price == 0:
-                price = decimal.Decimal(net) / shares
+                price = decimal.Decimal(net) / quantity
+            else:
+                price = decimal.Decimal(price)
             if fee == "N/A":
                 fee = decimal.Decimal(0)
             else:
                 fee = decimal.Decimal(fee)
 
-            is_short_option = False
             if order_type.lower() in OPTION_TRANSACTIONS:
                 price = price * SHARES_PER_CONTRACT
-            elif order_type.lower() in SHORT_OPTION_TRANSACTIONS:
-                price = price * SHARES_PER_CONTRACT
-                is_short_option = True
 
-            transaction = Transaction(
-                index, date, symbol, is_short_option, name, shares, price, fee
-            )
+            rows.append([date, order_type, symbol, cusip,
+                        desc, quantity, price, fee, net])
+        rows.sort(key=functools.cmp_to_key(compare_function))
 
-            if order_type.lower() in OPEN_LOT_TRANSACTIONS:
-                lot = Lot(transaction)
-                open_lots[symbol].append(lot)
-                logging.debug(f"Added lot: {lot}")
-            elif order_type.lower() in CLOSE_LOT_TRANSACTIONS:
-                if (fiscal_year == 0) or (fiscal_year == date.year):
-                    sales[symbol].append(transaction)
-                logging.debug(f"Added sale: {transaction}")
+    for index, (date, order_type, symbol, cusip, desc, quantity, price, fee, net) in enumerate(rows):
+
+        is_short_option = order_type.lower() in SHORT_OPTION_TRANSACTIONS
+
+        transaction = Transaction(
+            index, date, symbol, is_short_option, name, quantity, price, fee)
+
+        if order_type.lower() in OPEN_LOT_TRANSACTIONS:
+            lot = Lot(transaction)
+            open_lots[symbol].append(lot)
+            logging.debug(f"Added lot: {lot}")
+        elif order_type.lower() in CLOSE_LOT_TRANSACTIONS:
+            if (fiscal_year == 0) or (fiscal_year == date.year):
+                sales[symbol].append(transaction)
+            logging.debug(f"Added sale: {transaction}")
 
     return open_lots, sales
